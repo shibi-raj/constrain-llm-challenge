ec6e72e20412e9cfc802557066ad60102268f25f
Shibi Rajagopalan <shibi.1729@gmail.com>
2026-08-17 09:18:05 -0400

    Update LLM config.

--- src/constrain_llm/config/settings.py
+++ src/constrain_llm/config/settings.py
@@ -1,11 +1,12 @@
 
 # Final submission:
-# LLM_PROVIDER = "openrouter"
-# LLM_MODEL = "openai/gpt-oss-120b:exacto"
+LLM_PROVIDER = "openrouter"
+LLM_MODEL = "openai/gpt-oss-120b:exacto"
+
 
 # Local development option
-LLM_PROVIDER = "ollama"
-LLM_MODEL = "qwen3:8b"
+# LLM_PROVIDER = "ollama"
+# LLM_MODEL = "qwen3:8b"
 
 # Try for free – rate limited
 # LLM_PROVIDER = "openrouter"
